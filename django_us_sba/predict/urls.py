11d7f2bd4edd207a6821b9360e036b4e177db949
Christian Martiny <christian.martiny@outlook.com>
2024-02-23 12:37:00 +0100

    init templates + debug server avec les variables .env

--- django_us_sba/predict/urls.py
+++ django_us_sba/predict/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from predict import views
 
 urlpatterns = [
-    path('', views.homepage, name='root_homepage'),
+    path('', views.homepage, name='predict_homepage'),
 ]
