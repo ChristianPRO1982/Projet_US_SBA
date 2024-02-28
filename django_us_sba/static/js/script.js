46baa41857af2feb894418db591e2cdd3615adc4
Christian Martiny <christian.martiny@outlook.com>
2024-02-28 14:25:30 +0100

    PostGres OK ?

--- django_us_sba/static/js/script.js
+++ django_us_sba/static/js/script.js
@@ -6,3 +6,10 @@ const menu = document.querySelector('#menu');
 hamburger.addEventListener('click', function () {
     menu.classList.toggle('hidden')
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+    var overlay = document.getElementById("overlay");
+    setTimeout(function() {
+        overlay.classList.add('hidden'); // Ajoute la classe 'hidden' après une seconde
+    }, 1000);
+});
