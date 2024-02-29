44cb486194320cdd1f62785309e2becaed7090c8
Christian Martiny <christian.martiny@outlook.com>
2024-02-29 10:51:48 +0100

    nouvelle nav bar

--- django_us_sba/us_sba/settings.py
+++ django_us_sba/us_sba/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    "livereload",
     'django.contrib.staticfiles',
     'root',
     'predict',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'us_sba.urls'
