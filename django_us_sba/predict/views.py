82c96de3792603e3544692146e64e85f8b101962
Christian Martiny <christian.martiny@outlook.com>
2024-03-04 09:53:55 +0100

    erreur de l'espace & page d'accueil améliorée pour le js

--- django_us_sba/predict/views.py
+++ django_us_sba/predict/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def predict_homepage(request):
     return render(request, 'predict/predict_homepage.html')
--- django_us_sba/root/views.py
+++ django_us_sba/root/views.py
@@ -8,6 +8,9 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 
 
+def error_404(request, exception):
+    return render(request, 'root/404.html', status=404)
+
 def root_homepage(request):
     return render(request, 'root/root_homepage.html')
 
