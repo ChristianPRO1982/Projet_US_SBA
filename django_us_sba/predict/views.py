6cbfd6921e7605e06a9ebd88fc443124a7e0f2ea
Christian Martiny <christian.martiny@outlook.com>
2024-03-05 09:33:50 +0100

    .

--- django_us_sba/predict/views.py
+++ django_us_sba/predict/views.py
@@ -5,3 +5,7 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def predict_homepage(request):
     return render(request, 'predict/predict_homepage.html')
+
+@login_required
+def new_process(request):
+    return render(request, 'predict/new_process.html')
--- django_us_sba/us_sba/settings.py
+++ django_us_sba/us_sba/settings.py
@@ -133,6 +133,7 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 
 # AUTH_USER_MODEL = "root.User"
